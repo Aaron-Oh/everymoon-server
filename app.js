2425eb0c5b06b3b6fed2b98bf379137f3455089b
Jay <60384440+JayM-Oh@users.noreply.github.com>
2023-03-30 23:46:00 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
-const tf = require('@tensorflow/tfjs-node');
-
 const express = require('express');
+const tf = require('@tensorflow/tfjs-node');
+const multer = require('multer');
+const upload = multer();
 const app = express();
-const port = process.env.PORT || 8080;
 
-// 모델 로드
-async function loadModel() {
-  const model = await tf.loadLayersModel('file://./tf_js/model.json');
-  return model;
-}
+// 모델 로딩
+const model = await tf.loadLayersModel('file://./tf_js/model.json');
 
-// 이미지 예측
-async function predict(model, imagePath) {
-  const image = await tf.node.decodeImage(tf.node.readFileSync(imagePath));
-  const processedImage = image.expandDims(0).toFloat().div(tf.scalar(255));
-  const prediction = await model.predict(processedImage).data();
-  const classId = prediction.indexOf(Math.max(...prediction));
-  const classNames = ['large', 'medium'];
-  return classNames[classId];
-}
-
-// API 엔드포인트
-app.get('/predict', async (req, res) => {
+// POST /predict 요청 처리
+app.post('/predict', upload.single('image'), async (req, res) => {
   try {
-    const imagePath = req.query.image_path;
-    const model = await loadModel();
-    const prediction = await predict(model, imagePath);
-    res.send(prediction);
+    // 요청으로부터 이미지 데이터 추출
+    const imgData = req.file.buffer;
+
+    // 이미지를 텐서로 변환
+    const tensor = tf.node.decodeImage(imgData);
+
+    // 이미지 크기 조정 및 정규화
+    const resized = tf.image.resizeBilinear(tensor, [224, 224]).div(255);
+
+    // 모델에 이미지를 입력하여 예측 수행
+    const output = model.predict(resized);
+
+    // 예측 결과 추출 및 응답 전송
+    const predictions = output.arraySync()[0];
+    const prediction = predictions.indexOf(Math.max(...predictions));
+
+    res.json({
+      result: (prediction === 0) ? 'medium' : 'large'
+    });
   } catch (error) {
-    console.log(error);
-    res.status(500).send('Error');
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
-// 서버 시작
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}...`);
-});
-
-// 모델 언로드
-process.on('SIGINT', () => {
-  tf.tidy(() => model.dispose());
-  console.log('model disposed.');
-  process.exit();
+// 서버 구동
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
 });
