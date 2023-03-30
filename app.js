6bd582d23d22ba3124f33b8cf2358616904d675c
Jay <60384440+JayM-Oh@users.noreply.github.com>
2023-03-30 21:19:46 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const tf = require('@tensorflow/tfjs-node');
+const multer = require('multer');
+
+const app = express();
+const port = 8080;
+
+// 모델 로드
+const model = await tf.loadGraphModel('file://./tf_js/model.json');
+
+// 이미지 처리 및 예측
+const upload = multer().single('image');
+app.post('/predict', upload, async (req, res) => {
+  const img = tf.node.decodeImage(req.file.buffer);
+  const prediction = model.predict(img);
+  const result = prediction.dataSync();
+  res.json(result);
+});
+
+// 서버 시작
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
