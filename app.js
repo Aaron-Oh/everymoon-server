975805040240b8e575c9c70b5dfc51d3c7a8c26b
Jay <60384440+JayM-Oh@users.noreply.github.com>
2023-03-30 23:41:01 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,38 +1,46 @@
-const express = require('express');
 const tf = require('@tensorflow/tfjs-node');
-const path = require('path');
 
+const express = require('express');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-const modelPath = path.join(__dirname, 'tf_js', 'model.json');
-
+// 모델 로드
 async function loadModel() {
-  const model = await tf.loadLayersModel(`file://${modelPath}`);
-  console.log('model loaded');
+  const model = await tf.loadLayersModel('file://./tf_js/model.json');
   return model;
 }
 
-let modelPromise = null;
+// 이미지 예측
+async function predict(model, imagePath) {
+  const image = await tf.node.decodeImage(tf.node.readFileSync(imagePath));
+  const processedImage = image.expandDims(0).toFloat().div(tf.scalar(255));
+  const prediction = await model.predict(processedImage).data();
+  const classId = prediction.indexOf(Math.max(...prediction));
+  const classNames = ['large', 'medium'];
+  return classNames[classId];
+}
 
-app.get('/', async (req, res) => {
-  if (!modelPromise) {
-    modelPromise = loadModel();
-  }
-
+// API 엔드포인트
+app.get('/predict', async (req, res) => {
   try {
-    const model = await modelPromise;
-    const input = tf.ones([1, 224, 224, 3]);
-    const prediction = model.predict(input);
-    const output = prediction.dataSync();
-    const result = output[0] > output[1] ? 'large' : 'medium';
-    res.send(result);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+    const imagePath = req.query.image_path;
+    const model = await loadModel();
+    const prediction = await predict(model, imagePath);
+    res.send(prediction);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Error');
   }
 });
 
+// 서버 시작
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
+});
+
+// 모델 언로드
+process.on('SIGINT', () => {
+  tf.tidy(() => model.dispose());
+  console.log('model disposed.');
+  process.exit();
 });
